ee10b3083e9b02e79fe3bb42e2d730d1a0e170f3
sikor-22 <72802045+sikor-22@users.noreply.github.com>
2021-03-16 10:29:55 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -5,6 +5,23 @@
 #include <math.h>
 
 
+
+//utility function to calculate total distance given graph matrix "graph" of size "size" and inversion of nodes "order"
+int calc_distance(int* order,int** graph, int size)
+{
+    int i, current_node, next_node;
+    int distance = 0;
+    for(i = 0; i < size-1; i++)
+    {
+        current_node = order[i];
+        next_node = order[i+1];
+        distance += graph[current_node][next_node];
+    }
+    return (distance + graph[order[size-1]][order[0]]);
+}
+
+
+
 //utility function to create graph from given file
 int** get_graph(char* entry_file, int* size)
 {
@@ -13,18 +30,22 @@ int** get_graph(char* entry_file, int* size)
     {
         exit(-1);
     }
+
+
     fseek(city_file, 0L, SEEK_END);
     int size_of_file = ftell(city_file);
     fseek(city_file, 0L, SEEK_SET);
+    
     char* buff = calloc(size_of_file, sizeof(char));
     char* endptr;
-    //char* buff_tmp = calloc(size_of_file, sizeof(char));
+
     fgets(buff, size_of_file, city_file);
     int amnt_nodes = strtol(buff, &endptr, 10);
     *size = amnt_nodes;
-    //printf("%d", amnt_nodes);
+    
     int** return_graph = (int**) calloc(amnt_nodes, sizeof(int*));
     int i, j, tmp;
+    //memory alloc
     for(i = 0; i < amnt_nodes; i++)
     {
         return_graph[i] = (int*) calloc(amnt_nodes, sizeof(int*));
@@ -33,6 +54,8 @@ int** get_graph(char* entry_file, int* size)
             return_graph[i][j] = 0;
         }
     }
+
+    //scan values
     int counter = 0;
     while(counter < (int)pow(amnt_nodes, 2))
     {
@@ -49,4 +72,6 @@ int main(int argc, char **argv)
     int amnt_nodes;
     char* file_name = "test.txt";
     int **t = get_graph(file_name, &amnt_nodes);
+    int order[6] = {0, 1, 2, 3, 4, 5};
+    printf("%d", calc_distance(order, t, amnt_nodes));
 }
